099aa15c7243e3b520959e2c4260d28a89e82a70
koresachin7 <koresachin7@gmail.com>
2023-03-16 14:19:50 +0530

    Add : - use of input and output function

---
+++ inputAndOutput/inputAndPrint.py
@@ -0,0 +1,11 @@
+# var = input("Enter value : -")  # input function assigning value in variable
+var = int(input("Enter value : -"))
+'''you can change value type by using type casting
+ int for integer and str for string etc..
+ ,Note : - we can use multiple type in one function "eval"'''
+
+print(var)  # print function display output in consoler
+print(type(var))  # type function show type
+print("hello", "sachin", "kapil", sep="-")  # separated value by "-"
+print("hello", end="")
+print("world")  # end space
